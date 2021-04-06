d9eabe74a62b77605973ea94fd5e2d9f299d0deb
Jim Moua <jimmoua24@gmail.com>
2021-04-06 05:52:20 -0500

    media functions to incorporate context as req dat

--- ddb_helpers.rb
+++ ddb_helpers.rb
@@ -1,5 +1,6 @@
 require 'aws-sdk'
 require 'json'
+require 'uuid'
 
 Aws.config.update(
   endpoint: 'https://dynamodb.us-east-2.amazonaws.com',
@@ -23,18 +24,21 @@ end
 
 # POST /api/new
 def create_media_entry(event:, context:)
+  rq = event
   ddb = Aws::DynamoDB::Client.new
+  mid = UUID.new.generate
   ddb.put_item({
                  table_name: 'Medias',
                  item: {
-                   id: UUID.new.generate,
-                   title: title,
-                   type: type,
-                   status: status
+                   id: mid,
+                   title: rq['title'],
+                   type: rq['type'],
+                   status: rq['status']
                  }
                })
   {
-    statusCode: 200
+    statusCode: 200,
+    mediaID: mid,
   }
 rescue StandardError => e
   {
@@ -45,8 +49,24 @@ end
 
 # POST /api/delete
 def delete_media_entry(event:, context:)
+  rq = event
   ddb = Aws::DynamoDB::Client.new
-  ddb.delete_item({ table_name: 'Medias', key: { id: id } })
+
+  # Search for item, if not exists, return 404
+  resp = ddb.get_item(
+    key: {
+      "id" => rq['id']
+    },
+    table_name: 'Medias'
+  )
+  if resp[:item].nil?
+    return {
+      statusCode: 404
+    }
+  end
+
+  # Delete item
+  ddb.delete_item({ table_name: 'Medias', key: { id: rq['id'] } })
   {
     statusCode: 200
   }
@@ -57,24 +77,39 @@ rescue StandardError => e
   }
 end
 
-# TODO POST /api/update
+# POST /api/update
 def update_media_entry(event:, context:)
-  db_client = Aws::DynamoDB::Client.new
+  rq = event
+  ddb = Aws::DynamoDB::Client.new
+
+  # Search for item, if not exists, return 404
+  resp = ddb.get_item(
+    key: {
+      "id" => rq['id']
+    },
+    table_name: 'Medias'
+  )
+  if resp[:item].nil?
+    return {
+      statusCode: 404
+    }
+  end
+
   new_data = {
     title: {
-      value: data[:title],
+      value: rq['title'],
       action: 'PUT'
     },
     status: {
-      value: data[:status],
+      value: rq['status'],
       action: 'PUT'
     },
     type: {
-      value: data[:type],
+      value: rq['type'],
       action: 'PUT'
     }
   }
-  db_client.update_item({ table_name: 'Medias', key: { id: id }, attribute_updates: new_data })
+  ddb.update_item({ table_name: 'Medias', key: { id: rq['id'] }, attribute_updates: new_data })
 rescue StandardError => e
   {
     statusCode: 500,
